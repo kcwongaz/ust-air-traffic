1. Added altitude check for FR24 2. Corrected FR24 time-distance calculation 3. Edited the manual clusters

characteristics/data-fr24_time_distance.py
@@ -4,10 +4,10 @@ import numpy as np
 from geopy import distance
 
 
-label = "china"
+label = "clean"
 datadir = f"/home/kc/Research/air_traffic/data/fr24_{label}/"
-savedir = "/home/kc/Research/air_traffic/data/fr24_stat"
-savename = f"{savedir}/time_distance_{label}_filtered.txt"
+savedir = "/home/kc/Research/air_traffic/data/fr24_clean"
+savename = f"{savedir}/time_distance_{label}.txt"
 
 
 # --------------------------------------------------------------------------- #
@@ -44,8 +44,21 @@ for subdir, dirs, files in os.walk(datadir):
         if len(df) == 0:
             continue
 
+        # First point in the square
         first = df.iloc[0]
-        last = df.iloc[-1]
+
+        # Find the first landing point
+        # Get through the inital take-off stage
+        n = 0
+        while df["altitude"].iloc[n] == 0:
+            n += 1
+            # Also possible that the whole flight record is already landed
+            if n == len(df):
+                n = 0
+                break
+        df = df.iloc[n:]
+        df = df.loc[df["altitude"] == 0]
+        last = df.iloc[0]
 
         # Filter those points that are not in the square
         if (np.abs(last["latitude"] - hkia[0]) > 0.25) and \

characteristics/plot-time_distance_cluster.py
@@ -4,20 +4,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-fname = "/home/kc/Research/air_traffic/data/fr24_stat/time_distance_china_filtered.txt"
+fname = "/home/kc/Research/air_traffic/data/fr24_clean/time_distance_clean.txt"
 filters = {"lower_r": False, "upper_t": False}
 
 sdir = "/home/kc/Research/air_traffic/figures/2022-02-22"
 
+# boxes = [(23.2, 23.6, 113.2, 113.4), (22.5, 24.5, 111, 111.2),
+#          (20.2, 21.2, 111, 111.2), (25, 25.5, 113, 115),
+#          (21.5, 22.9, 117, 117.5), (19.1, 20.1, 117.3, 117.5),
+#          (19, 19.2, 112, 113.2), (19, 19.2, 113.9, 114.8)]
+
 boxes = [(23.2, 23.6, 113.2, 113.4), (22.5, 24.5, 111, 111.2),
          (20.2, 21.2, 111, 111.2), (25, 25.5, 113, 115),
-         (21.5, 22.9, 117, 117.5), (19.1, 20.1, 117.3, 117.5)]
+         (21.5, 25, 117, 117.5), (19.1, 20.1, 117.3, 117.5),
+         (19, 19.2, 111.8, 113.5), (19, 19.2, 113.5, 116.2)]
 
-sname = "time_distance"
+colors = ["darkviolet", "deeppink", "darkorange", "limegreen",
+          "teal", "mediumblue", "goldenrod", "crimson"]
+
+sname = "speed_distance"
 
 # --------------------------------------------------------------------------- #
 df = pd.read_csv(fname, header=0)
-df = df.loc[df["delta_t_sec"] / 60 < 800]
+# df = df.loc[df["delta_t_sec"] / 60 < 800]
 
 plt.rcParams.update({
     "font.family": "serif",
@@ -25,18 +34,18 @@ plt.rcParams.update({
 )
 
 if sname == "time_distance":
-    fig, axes = plt.subplots(ncols=3, nrows=2, figsize=(8, 5))
+    fig, axes = plt.subplots(ncols=4, nrows=2, figsize=(8, 4))
     for n, box in enumerate(boxes):
 
-        ax = axes[int(n // 3), int(n % 3)]
+        ax = axes[int(n // 4), int(n % 4)]
         df_sub = df.loc[(df["lat_i"] >= box[0]) & (df["lat_i"] <= box[1]) &
                         (df["lon_i"] >= box[2]) & (df["lon_i"] <= box[3])]
 
         delta_r = df_sub["delta_r_km"].to_numpy()
         delta_t = df_sub["delta_t_sec"].to_numpy() / 60
 
-        ax.scatter(delta_r, delta_t, s=20,
-                   c="blue", marker="o", alpha=0.12)
+        ax.scatter(delta_r, delta_t, s=10,
+                   c=colors[n], marker="o", alpha=0.15)
 
         ax.set_ylim(0, 200)
 
@@ -49,10 +58,10 @@ if sname == "time_distance":
 
 
 elif sname == "speed_distance":
-    fig, axes = plt.subplots(ncols=3, nrows=2, figsize=(8, 5))
+    fig, axes = plt.subplots(ncols=4, nrows=2, figsize=(8, 4))
     for n, box in enumerate(boxes):
 
-        ax = axes[int(n // 3), int(n % 3)]
+        ax = axes[int(n // 4), int(n % 4)]
         df_sub = df.loc[(df["lat_i"] >= box[0]) & (df["lat_i"] <= box[1]) &
                         (df["lon_i"] >= box[2]) & (df["lon_i"] <= box[3])]
 
@@ -61,12 +70,37 @@ elif sname == "speed_distance":
         delta_t = df_sub["delta_t_sec"].to_numpy() / 60
         speed = delta_r / delta_t
 
-        ax.scatter(delta_r, speed, s=20,
-                   c="red", marker="o", alpha=0.12)
+        ax.scatter(delta_r, speed, s=10,
+                   c=colors[n], marker="o", alpha=0.15)
 
         if n == 0:
             ax.set_xlabel("Distance (km)", fontsize=10)
             ax.set_ylabel("Average Speed (km/min)", fontsize=10)
+
+    plt.tight_layout()
+    plt.savefig(f"{sdir}/{sname}_clusters.png", dpi=300)
+
+
+elif sname == "speed_distribution":
+    fig, axes = plt.subplots(ncols=4, nrows=2, figsize=(8, 4))
+    for n, box in enumerate(boxes):
+
+        ax = axes[int(n // 4), int(n % 4)]
+        df_sub = df.loc[(df["lat_i"] >= box[0]) & (df["lat_i"] <= box[1]) &
+                        (df["lon_i"] >= box[2]) & (df["lon_i"] <= box[3])]
+
+        df = df.loc[df["delta_t_sec"] > 0]
+        delta_r = df_sub["delta_r_km"].to_numpy()
+        delta_t = df_sub["delta_t_sec"].to_numpy() / 60
+        speed = delta_r / delta_t
+
+        ax.hist(speed, bins=50, range=(0, 15), color=colors[n])
+        ax.set_xlim(0, 15)
+        ax.set_xticks(np.arange(0, 16, 5))
+
+        if n == 0:
+            ax.set_xlabel("Average Speed (km/min)", fontsize=10)
+            ax.set_ylabel("Count", fontsize=10)
 
     plt.tight_layout()
     plt.savefig(f"{sdir}/{sname}_clusters.png", dpi=300)