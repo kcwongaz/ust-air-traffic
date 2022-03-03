e19f25454f22469b97b2bb3839b2ac7db1604c55
KC Wong <kcwongaz@gmail.com>
2022-03-03 16:27:05 +0800

    Refactor: Refactor autrocrrelation calculation

--- characteristics/arrivial/arrivial_rate.py
+++ characteristics/arrivial/arrivial_rate.py
@@ -1,33 +1,77 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
-fname = "/home/kc/Research/air_traffic/data/fr24_clean/time_distance.txt"
+def arrivial_rate(arr_times, dt, start_hour=-1, start_by="pad"):
+    """
+    Compute the arrivial rate by counting the number of arrivial in (t-dt, t).
+    Return the computed rate along with an array containing the
+    corresponding time values.
 
-sdir = "/home/kc/Research/air_traffic/figures/2022-03-01"
+    The start of the time array depends on the first arrivial time,
+    specifying start_hour != -1 force the time axis start at the wanted hour.
 
-plt.rcParams.update({
-    "font.family": "serif",
-    "font.sans-serif": ["Helvetica"]}
-)
+    start_by specify the method for offseting the time axis.
+        pad: Pad zeros up to the start point, so we don't lose data
+        cut: Cut data down to the start point, so we don't add extra zeros
+
+    (!) When UTC timestamps are inputted for arr_times, the resulting
+        counts will also be in UTC.
+    """
+
+    arr_times = np.sort(arr_times)
+    first = arr_times[0]
+
+    # Compute the first point of the time axis
+    if start_hour == -1:
+        t0 = first
+    else:
+        first_sec = first % 86400
+        start_sec = 3600*start_hour
+        delta = first_sec - start_sec
+
+        if start_by == "pad":
+            if delta >= 0:
+                t0 = first - delta
+            else:
+                t0 = first - 86400 - delta
+
+        elif start_by == "cut":
+            if delta <= 0:
+                t0 = first - delta
+            else:
+                t0 = first + 86400 - delta
+
+    # The first valid time point is t0 + dt because we have no data before t0
+    t_axis = np.arange(t0 + dt, arr_times[-1] + dt, dt)
+
+    # Compute the arrivial count by histogram
+    bins = np.insert(t_axis, 0, t0)
+    count, _ = np.histogram(arr_times, bins)
+
+    return count, t_axis
 
 
-# --------------------------------------------------------------------------- #
-def partition(timeseries, part_size, window):
+def fold_timeseries(timeseries, dt, foldsize):
+    """
+    Fold a timeseries onto a given time-window size
+    """
 
-    n = round(part_size / window)
+    n = round(foldsize / dt)
     total_len = int(len(timeseries) / n)
 
-    partitioned = [timeseries[k*n:(k+1)*n] for k in range(total_len)]
-    return partitioned
+    folded = [timeseries[k*n:(k+1)*n] for k in range(total_len)]
+    return folded
 
 
 def autocorr(timeseries, dt, period):
+    """
+    Compute the autocorrelation coefficient for a periodic time series
+    """
 
     n = round(period / dt)
     n_period = int(len(timeseries) / n)
-    c_matrix = np.zeros((n, n))
+    a_matrix = np.zeros((n, n))
 
     mean = np.zeros(n)
     var = np.zeros(n)
@@ -41,122 +85,8 @@ def autocorr(timeseries, dt, period):
             sample = [timeseries[m*n + i] * timeseries[m*n + i + j]
                       for m in range(n_period-1)]
 
-            # c_matrix[i, j] = np.mean(sample)
-
             j_mod = (i+j) % n
-            c_matrix[i, j] = np.mean(sample) - mean[i]*mean[j_mod]
-            c_matrix[i, j] /= np.sqrt(var[i] * var[j_mod])
+            a_matrix[i, j] = np.mean(sample) - mean[i]*mean[j_mod]
+            a_matrix[i, j] /= np.sqrt(var[i] * var[j_mod])
 
-    return c_matrix
-
-
-# --------------------------------------------------------------------------- #
-# Time window size to commute the Possion rate (in unit of second)
-window = 60 * 15
-year = 2017
-
-
-df = pd.read_csv(fname, header=0)
-df["year"] = pd.to_datetime(df["t_f"], unit="s").dt.year
-df["month"] = pd.to_datetime(df["t_f"], unit="s").dt.month
-df = df.loc[df["year"] == year]
-# df = df[df["month"].isin([7])]
-
-
-arrivial_time = df["t_f"].to_numpy()
-arrivial_time.sort()
-print(pd.to_datetime(df["t_f"].iloc[0], unit="s"))
-
-
-# t0 = pd.to_datetime(f"{year}-01-01", format="%Y-%m-%d").timestamp()
-# t_axis = np.arange(t0 + window, arrivial_time[-1], window)
-# bins = np.insert(t_axis, 0, t0)
-# rate, _ = np.histogram(arrivial_time, bins)
-
-
-t_axis = np.arange(arrivial_time[0] + window, arrivial_time[-1], window)
-bins = np.insert(t_axis, 0, arrivial_time[0])
-rate, _ = np.histogram(arrivial_time, bins)
-
-
-fig, ax = plt.subplots()
-
-# # --------------------------------------------------------------------------- #
-# rate_day = partition(rate, 3600*24, window)
-# day_axis = np.arange(len(rate_day[0])) / 4
-
-# cmap = plt.cm.get_cmap("rainbow", 20)
-# for i, curve in enumerate(rate_day):
-
-#     if np.sum(curve) == 0:
-#         continue
-#     ax.plot(day_axis, curve, color=cmap(i % 20), alpha=0.75)
-
-# ax.set_xticks(range(0, 25, 2))
-# ax.set_xlabel("Time mod 24 Hour (Hour)", fontsize=14)
-# ax.set_ylabel("Arrivial Count", fontsize=14)
-# ax.set_title(f"{year}", fontsize=18)
-# ax.set_ylim(-0.5, 12)
-# ax.vlines(16.5, ymin=-0.5, ymax=12, color="black", lw=2.5)
-
-# plt.savefig(f"{sdir}/timeseries_{year}.png", dpi=300)
-# plt.tight_layout()
-
-# --------------------------------------------------------------------------- #
-r_avg = np.average(rate)
-# print(r_avg)
-# rate = rate - r_avg
-
-
-# spectrum = np.abs(np.fft.rfft(rate))
-# spectrum /= np.max(spectrum)
-# freq = np.fft.rfftfreq(len(rate), d=window) * 3600
-# period = 1/(freq)
-
-# # Frequency domain
-# ax.plot(freq, spectrum, color="blue")
-# ax.set_xlabel("Frequency $f$ (1/Hour)", fontsize=16)
-# ax.set_ylabel(r"Spectral Component $|\tilde{\lambda}(f)|$", fontsize=16)
-
-# plt.savefig(f"{sdir}/spectrum_frequency_{year}.png", dpi=300)
-
-
-# Period domain
-# n = 50
-# ax.plot(period[n:], spectrum[n:], color="blue")
-# ax.set_xlabel("Period $T$ (Hour)", fontsize=16)
-# ax.set_ylabel(r"Spectral Component $|\tilde{\lambda}(T)|$", fontsize=16)
-
-# ax.set_title(f"Fourier Spectrum ({year})", fontsize=20)
-# plt.savefig(f"{sdir}/spectrum_period_{year}.png", dpi=300)
-
-
-# --------------------------------------------------------------------------- #
-
-corr = autocorr(rate, dt=window, period=86400)
-t_axis = np.arange(0, 24, window/3600)
-# ax.plot(t_axis, corr[0])
-
-
-# for i in range(len(corr[0])):
-#     ax.plot(t_axis, corr[i])
-
-im = ax.imshow(corr, cmap="plasma", extent=(0, 24, 0, 24), origin="lower",
-               vmin=0, vmax=1)
-
-ax.set_ylabel("Time $t$ (Hours from 00:00)", fontsize=12)
-ax.set_xlabel("Time Difference $T$ (Hour)", fontsize=12)
-
-ax.set_xticks(range(0, 25, 2))
-ax.set_yticks(range(0, 25, 2))
-ax.set_title(f"Arrivial Rate Autocorrection ({year})", fontsize=14)
-
-cax = plt.axes([0.91, 0.15, 0.02, 0.7])
-plt.colorbar(im, cax)
-
-plt.tight_layout()
-# plt.savefig(f"{sdir}/autocorrection_{year}.png", dpi=300)
-
-
-# --------------------------------------------------------------------------- #
-plt.show()
+    return a_matrix
---
+++ characteristics/arrivial/plot-autocorrelation.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from arrivial_rate import arrivial_rate, autocorr
+
+
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.sans-serif": ["Helvetica"]}
+)
+
+fname = "/home/kc/Research/air_traffic/data/fr24_clean/time_distance.txt"
+sdir = "/home/kc/Research/air_traffic/figures/2022-03-01"
+
+dt = 60 * 15
+year = 2018
+
+# --------------------------------------------------------------------------- #
+# Read data
+
+df = pd.read_csv(fname, header=0)
+df["year"] = pd.to_datetime(df["t_f"], unit="s").dt.year
+df["month"] = pd.to_datetime(df["t_f"], unit="s").dt.month
+df = df.loc[df["year"] == year]
+# df = df[df["month"].isin([7])]
+
+arr_times = df["t_f"].to_numpy()
+lambda_t, _ = arrivial_rate(arr_times, dt, 0)
+a_matrix = autocorr(lambda_t, dt, 86400)
+
+
+# --------------------------------------------------------------------------- #
+# Draw
+
+fig, ax = plt.subplots()
+t_axis = np.arange(dt/3600, 24, dt/3600)  # Time axis in hour
+
+
+im = ax.imshow(a_matrix, cmap="plasma", extent=(0, 24, 0, 24), origin="lower",
+               vmin=0, vmax=1)
+
+ax.set_ylabel("Time $t$ (Hours from 00:00)", fontsize=12)
+ax.set_xlabel("Time Difference $T$ (Hour)", fontsize=12)
+
+ax.set_xticks(range(0, 25, 2))
+ax.set_yticks(range(0, 25, 2))
+ax.set_title(f"Arrivial Rate Autocorrection ({year})", fontsize=14)
+
+cax = plt.axes([0.91, 0.15, 0.02, 0.7])
+plt.colorbar(im, cax)
+
+plt.tight_layout()
+# plt.savefig(f"{sdir}/autocorrection_{year}.png", dpi=300)
+
+
+# --------------------------------------------------------------------------- #
+plt.show()
